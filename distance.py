2bb292ceb877d0d0a5b81fcd6788363be816e07f
abdulmomen96 <abdu.galga@gmail.com>
2021-12-12 15:30:14 +0200

    Adding Videos2 bug fix + storing images

--- distance.py
+++ distance.py
@@ -11,9 +11,9 @@ print(sequence)
 sequence.sort(key=lambda s: get_num(s))
 
 start = np.zeros((256, 256, 3))
-
+first_image = np.zeros((256, 256, 3))
 frames = []
-for img in sequence:
+for img, i in zip(sequence, range(len(sequence))):
     temp = np.zeros((256, 256, 3))
     img = cv2.imread(img)
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -53,10 +53,18 @@ for img in sequence:
         #cv2.line(img, (x - line_length, y), (x + line_length, y), cross_color, 2)
         #cv2.line(img, (x, y - line_length), (x, y + line_length), cross_color, 2)
 
+    if i == 0:
+        first_image = start
+
     start = (temp + start) % 256
+    #cv2.imshow('image', start)
+    #cv2.waitKey(0)
     frames.append(np.array(start))
-    cv2.imshow('image', start)
-    cv2.waitKey(0)
+
+cv2.imshow('image', start)
+cv2.waitKey(0)
+cv2.imshow('image', start - first_image)
+cv2.waitKey(0)
 cv2.imwrite('result.png', start)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('result.avi', fourcc, 70, (640, 480))
@@ -65,3 +73,62 @@ for image in frames:
     out.write(frame)
 
 out.release()
+
+start = cv2.imread('result.png', cv2.IMREAD_GRAYSCALE)
+start = np.ubyte((start / np.max(start)) * 255)
+
+#start = cv2.cvtColor(start, cv2.COLOR_RGB2GRAY)
+cv2.imshow('thresh', start)
+cv2.waitKey(0)
+original_image = start
+mask_image = start
+#mask_image = cv2.resize(mask_image, (original_image.shape[1], original_image.shape[0]))
+ret, thresh = cv2.threshold(mask_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+# cv2.imshow('thresh', thresh)
+# cv2.waitKey(0)
+
+kernel = np.ones((3, 3), np.uint8)
+opening = cv2.morphologyEx(mask_image, cv2.MORPH_OPEN, kernel, iterations=2)
+cv2.imshow('opening', opening)
+cv2.waitKey(0)
+
+# sure background area
+sure_bg = cv2.dilate(opening, kernel, iterations=1)
+cv2.imshow('sure_bg', sure_bg)
+cv2.waitKey(0)
+
+# Finding sure foreground area
+dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+cv2.imshow('dist_transform', sure_bg)
+cv2.waitKey(0)
+
+ret, sure_fg = cv2.threshold(dist_transform, 0.6 * dist_transform.max(), 255, 0)
+
+cv2.imshow('sure_fg', sure_fg)
+cv2.waitKey(0)
+
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+cv2.imshow('sure_fg2', sure_fg)
+cv2.waitKey(0)
+unknown = cv2.subtract(sure_bg, sure_fg)
+cv2.imshow('unknown', unknown)
+cv2.waitKey(0)
+# Marker labelling
+ret, markers = cv2.connectedComponents(sure_fg)
+count = max(ret, 0)
+
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers + 1
+
+# Now, mark the region of unknown with zero
+markers[unknown == 255] = 0
+
+print(original_image.shape)
+print(markers.shape)
+markers = cv2.watershed(original_image, markers)
+original_image[markers == -1] = [255, 0, 0]
+#font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+#cv2.putText(original_image, 'concentration: ' + str(count), (10, 450), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+cv2.imshow('', original_image)
+print(count)
